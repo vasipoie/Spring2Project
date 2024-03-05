1c05686514de378d546e29a6a419efca5fa71861
Nahye Kim <poieoe09@gmail.com>
2024-03-05 19:43:28 +0900

    240305 파일업로드 등록, 리스트, 수정, 삭제

---
+++ Spring2Project/src/main/java/kr/or/ddit/mapper/IItemMapper.java
@@ -0,0 +1,15 @@
+package kr.or.ddit.mapper;
+
+import java.util.List;
+
+import kr.or.ddit.vo.Item;
+
+public interface IItemMapper {
+	public void create(Item item);
+	public List<Item> list();
+	public Item read(int itemId);
+	public String getPicture(int itemId);
+	public void modify(Item item);
+	public void remove(int itemId);
+
+}
