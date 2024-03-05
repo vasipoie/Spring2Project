04e9b68d1eeb4bf282cab529344b219c6f8679a5
Nahye Kim <poieoe09@gmail.com>
2024-03-05 11:45:43 +0900

    24.03.05 notice 공지사항 수정, 삭제, 검색, 목록 페이징 추가

--- Spring2Project/src/main/java/kr/or/ddit/service/impl/NoticeServiceImpl.java
+++ Spring2Project/src/main/java/kr/or/ddit/service/impl/NoticeServiceImpl.java
@@ -1,5 +1,7 @@
 package kr.or.ddit.service.impl;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Service;
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Service;
 import kr.or.ddit.ServiceResult;
 import kr.or.ddit.mapper.INoticeMapper;
 import kr.or.ddit.service.INoticeService;
+import kr.or.ddit.vo.PaginationInfoVO;
 import kr.or.ddit.vo.crud.NoticeVO;
 
 @Service
@@ -32,6 +35,40 @@ public class NoticeServiceImpl implements INoticeService {
 	public NoticeVO selectNotice(int boNo) {
 		noticeMapper.incrementHit(boNo);		//게시글 조회 수 증가
 		return noticeMapper.selectNotice(boNo); //게시글 번호에 해당하는 게시글 정보
+	}
+
+	@Override
+	public ServiceResult updateNotice(NoticeVO noticeVO) {
+		ServiceResult result = null;
+		int status = noticeMapper.updateNotice(noticeVO);
+		if(status > 0) {	//수정 성공
+			result = ServiceResult.OK;
+		}else {	//수정 실패
+			result = ServiceResult.FAILED;
+		}
+		return result;
+	}
+
+	@Override
+	public ServiceResult deleteNotice(int boNo) {
+		ServiceResult result = null;
+		int status = noticeMapper.deleteNotice(boNo);
+		if(status > 0) {	//삭제 성공
+			result = ServiceResult.OK;
+		}else {	//삭제 실패
+			result = ServiceResult.FAILED;
+		}
+		return result;
+	}
+
+	@Override
+	public int selectNoticeCount(PaginationInfoVO<NoticeVO> pagingVO) {
+		return noticeMapper.selectNoticeCount(pagingVO);
+	}
+
+	@Override
+	public List<NoticeVO> selectNoticeList(PaginationInfoVO<NoticeVO> pagingVO) {
+		return noticeMapper.selectNoticeList(pagingVO);
 	}
 
 }
