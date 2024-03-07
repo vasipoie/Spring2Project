2bc258df0cdab1ae843e7b00b4e1883103403612
Nahye Kim <poieoe09@gmail.com>
2024-03-07 17:38:54 +0900

    240307 파일업로드-notice 공지사항 서비스 - 등록에 업로드 된 파일 데이터 처리를 위한 HttpServletRequest - 회원가입 시 아이디 중복체크 idCheck - 회원가입 signup - 로그인 체크 loginCheck

--- Spring2Project/src/main/java/kr/or/ddit/service/INoticeService.java
+++ Spring2Project/src/main/java/kr/or/ddit/service/INoticeService.java
@@ -2,17 +2,25 @@ package kr.or.ddit.service;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import kr.or.ddit.ServiceResult;
 import kr.or.ddit.vo.PaginationInfoVO;
+import kr.or.ddit.vo.crud.NoticeMemberVO;
 import kr.or.ddit.vo.crud.NoticeVO;
 
 public interface INoticeService {
 
-	public ServiceResult insertNotice(NoticeVO noticeVO);
+	public ServiceResult insertNotice(HttpServletRequest req, NoticeVO noticeVO);
 	public NoticeVO selectNotice(int boNo);
 	public ServiceResult updateNotice(NoticeVO noticeVO);
 	public ServiceResult deleteNotice(int boNo);
 	public int selectNoticeCount(PaginationInfoVO<NoticeVO> pagingVO);
 	public List<NoticeVO> selectNoticeList(PaginationInfoVO<NoticeVO> pagingVO);
+	
+	//파일업로드 notice - 로그인
+	public ServiceResult idCheck(String memId);
+	public ServiceResult signup(HttpServletRequest req, NoticeMemberVO memberVO);
+	public NoticeMemberVO loginCheck(NoticeMemberVO memberVO);
 
 }
