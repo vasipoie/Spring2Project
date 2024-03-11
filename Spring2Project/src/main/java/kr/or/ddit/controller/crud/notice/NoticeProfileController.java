7bcd99b59dd7264cedcb4a85797839cdecabfe6c
Nahye Kim <poieoe09@gmail.com>
2024-03-11 21:23:26 +0900

    240308 파일업로드-notice - 회원 가져오기 - 프로필 수정

--- Spring2Project/src/main/java/kr/or/ddit/controller/crud/notice/NoticeProfileController.java
+++ Spring2Project/src/main/java/kr/or/ddit/controller/crud/notice/NoticeProfileController.java
@@ -1,15 +1,90 @@
 package kr.or.ddit.controller.crud.notice;
 
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import kr.or.ddit.ServiceResult;
+import kr.or.ddit.service.INoticeService;
+import kr.or.ddit.vo.crud.NoticeMemberVO;
 
 @Controller
 @RequestMapping("/notice")
 public class NoticeProfileController {
 	
+	@Inject
+	private INoticeService noticeService;
+	
 	@GetMapping(value="/profile.do")
-	public String noticeProfile() {
-		return "notice/profile";
+	public String noticeProfile(
+			HttpSession session, RedirectAttributes ra, Model model
+			) {
+		
+		String goPage = "";
+		
+		//SessionInfo : 로그인 한 멤버 정보가 담겨있음
+		NoticeMemberVO sessionMember = (NoticeMemberVO) session.getAttribute("SessionInfo");
+		
+		if(sessionMember == null) {
+			ra.addFlashAttribute("message", "로그인 후 이용가능합니다!");
+			return "redirect:/notice/login.do";
+		}
+		
+		NoticeMemberVO member = noticeService.selectMember(sessionMember.getMemId());
+		
+		if(member != null) {
+			model.addAttribute("member", member);
+			goPage = "notice/profile";
+		}else {
+			ra.addFlashAttribute("message", "로그인 후 이용가능합니다!");
+			goPage = "redirect:/notice/login.do";
+		}
+		return goPage;
 	}
+
+	@PostMapping(value="/profileUpdate.do")
+	public String noticeProfileUpdate(
+			HttpServletRequest req,
+			NoticeMemberVO memberVO, RedirectAttributes ra, Model model) {
+		
+		String goPage = "";
+		
+		ServiceResult result = noticeService.profileUpdate(req, memberVO);
+		
+		if(result.equals(ServiceResult.OK)) {
+			ra.addFlashAttribute("message", "회원정보 수정이 완료되었습니다!");
+			goPage = "redirect:/notice/profile.do";
+			
+		}else {
+			model.addAttribute("member", memberVO);
+			model.addAttribute("message", "서버에러, 다시 시도해주세요!");
+			goPage = "notice/profile";
+		}
+		return goPage;
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
