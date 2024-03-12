e6450f4be23fd0ad3b2a4a653ef4717c24043010
Nahye Kim <poieoe09@gmail.com>
2024-03-12 14:19:59 +0900

    240308 파일업로드-notice INoticeService.java - 공지사항 파일 관련 - 수정, 삭제 HttpServletRequest 추가

--- Spring2Project/src/main/java/kr/or/ddit/service/INoticeService.java
+++ Spring2Project/src/main/java/kr/or/ddit/service/INoticeService.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import kr.or.ddit.ServiceResult;
 import kr.or.ddit.vo.PaginationInfoVO;
+import kr.or.ddit.vo.crud.NoticeFileVO;
 import kr.or.ddit.vo.crud.NoticeMemberVO;
 import kr.or.ddit.vo.crud.NoticeVO;
 
@@ -13,8 +14,8 @@ public interface INoticeService {
 
 	public ServiceResult insertNotice(HttpServletRequest req, NoticeVO noticeVO);
 	public NoticeVO selectNotice(int boNo);
-	public ServiceResult updateNotice(NoticeVO noticeVO);
-	public ServiceResult deleteNotice(int boNo);
+	public ServiceResult updateNotice(HttpServletRequest req, NoticeVO noticeVO);
+	public ServiceResult deleteNotice(HttpServletRequest req, int boNo);
 	public int selectNoticeCount(PaginationInfoVO<NoticeVO> pagingVO);
 	public List<NoticeVO> selectNoticeList(PaginationInfoVO<NoticeVO> pagingVO);
 	
@@ -22,5 +23,12 @@ public interface INoticeService {
 	public ServiceResult idCheck(String memId);
 	public ServiceResult signup(HttpServletRequest req, NoticeMemberVO memberVO);
 	public NoticeMemberVO loginCheck(NoticeMemberVO memberVO);
+	public NoticeFileVO noticeDownload(int fileNo);
+	
+	public NoticeMemberVO selectMember(String memId);
+	public ServiceResult profileUpdate(HttpServletRequest req, NoticeMemberVO memberVO);
+	
+	public String idForget(NoticeMemberVO noticeMemberVO);
+	public String pwForget(NoticeMemberVO noticeMemberVO);
 
 }
